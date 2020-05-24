0bb6acf8b582919b4eff2e9b4d198d6eed47b7e9
Your Name <you@example.com>
2020-05-24 10:31:17 -0400

    Add exercise_4

---
+++ introduction_to_programming/exercise_4.py
@@ -0,0 +1,12 @@
+
+#getting width
+width = float(input("Enter width of field in feets >> "))
+
+#getting length
+length = float(input("Enter length of field in feets >> "))
+
+#calculating area in acres
+area = (length * width)/43560
+
+#printing area
+print('Area of field =', area, 'acres')
