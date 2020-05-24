ccd7db47d83ffd278f1fc16e925ed270ce04cc87
Your Name <you@example.com>
2020-05-24 10:30:59 -0400

    Add exercise_3

---
+++ introduction_to_programming/exercise_3.py
@@ -0,0 +1,12 @@
+
+#getting width
+width = float(input("Enter width of room in feets >> "))
+
+#getting length
+length = float(input("Enter length of room in feets >> "))
+
+#calculating area
+area = length * width
+
+#printing area
+print('Area of room =', area, 'sq.feets')
